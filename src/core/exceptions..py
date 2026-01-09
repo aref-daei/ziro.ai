1155b416be9f1368464e9d8018d213995bd49326
Aref Daei <aref.daei@outlook.com>
2026-01-09 22:18:20 +0330

    refactor: add ConnectionException to exceptions.py

--- src/core/exceptions..py
+++ src/core/exceptions..py
@@ -1,14 +1,2 @@
-class AudioExtractionError(RuntimeError):
-    pass
-
-
-class TranscriptionError(RuntimeError):
-    pass
-
-
-class UnsupportedModelError(RuntimeError):
-    pass
-
-
-class SubtitleAddError(RuntimeError):
+class ConnectionException(RuntimeError):
     pass
