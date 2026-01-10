196fe6006494b428ccfc0c07da0f8756e57f124d
Aref Daei <aref.daei@outlook.com>
2026-01-10 21:34:15 +0330

    fix: set version from 1.3.0-RC to 1.3.0 in main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 
 """
 Ziro.ai - Automated Subtitle Generation Application
-Version 1.3.0-RC
+Version 1.3.0
 Copyright (C) 2025  Aref Daei
 
 This program is free software: you can redistribute it and/or modify
