7916403bcccf3efcf8e47a777a39a8a9fa1a2e73
Aref Daei <aref.daei@outlook.com>
2026-03-01 02:03:17 +0330

    fix: rename config.py to constants.py

--- src/core/constants.py
+++ src/core/constants.py
@@ -7,19 +7,3 @@ PROJECT_AUTHOR = "Aref Daei"
 PROJECT_AUTHOR_EMAIL = "aref.daei@outlook.com"
 PROJECT_URL = "github.com/aref-daei/ziro.ai"
 PROJECT_LATEST_RELEASE_URL = "https://github.com/aref-daei/ziro.ai/releases/latest"
-
-# If DEBUG is False, disable logging completely
-DEBUG = False
-
-# Translate settings
-MAX_TRANSLATION_LENGTH = 512
-BATCH_SIZE = 8
-
-# FFmpeg settings
-AUDIO_FORMAT = "wav"
-AUDIO_CODEC = "pcm_s16le"
-AUDIO_RATE = 16000
-
-# Subtitle settings
-SRT_ENCODING = "utf-8"
-MAX_SUBTITLE_LENGTH = 42  # Maximum character in a line
