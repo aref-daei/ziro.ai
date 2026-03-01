cb7df91f68cc54bfce5dc0c881a47bf19ebb5d47
Aref Daei <aref.daei@outlook.com>
2026-03-01 12:18:50 +0330

    feat: add config file reading feature

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,8 +1,10 @@
+import yaml
+
 from core.constants import *
+from core.paths import PATHS
 
 
-# If DEBUG is False, disable logging completely
-DEBUG = False
+DEBUG = False  # If DEBUG is False, disable logging completely
 
 # Translate settings
 MAX_TRANSLATION_LENGTH = 512
@@ -10,9 +12,19 @@ BATCH_SIZE = 8
 
 # FFmpeg settings
 AUDIO_FORMAT = "wav"
-AUDIO_CODEC = "pcm_s16le"
 AUDIO_RATE = 16000
 
 # Subtitle settings
-SRT_ENCODING = "utf-8"
 MAX_SUBTITLE_LENGTH = 42  # Maximum character in a line
+
+config_path = PATHS["config"] / "config.yml"
+if config_path.exists():
+    with open(config_path) as f:
+        data = yaml.safe_load(f)
+
+        DEBUG = data["DEBUG"]
+        MAX_TRANSLATION_LENGTH = data["MAX_TRANSLATION_LENGTH"]
+        BATCH_SIZE = data["BATCH_SIZE"]
+        AUDIO_FORMAT = data["AUDIO_FORMAT"]
+        AUDIO_RATE = data["AUDIO_RATE"]
+        MAX_SUBTITLE_LENGTH = data["MAX_SUBTITLE_LENGTH"]
