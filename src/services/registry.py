c8c5a6de9c6646aec2e5847cbc458d81724ddf78
Aref Daei <aref.daei@outlook.com>
2026-07-25 00:36:21 +0330

    feat: implement service registry class

--- src/services/registry.py
+++ src/services/registry.py
@@ -1,6 +1,45 @@
 from src.core.app_config import AppConfig
+from .audio_extractor import AudioExtractorService
+from .subtitle_generator import SubtitleGeneratorService
+from .transcriber import TranscriberService
+from .transcriber.providers import WhisperTranscriber
+from .translator import TranslatorService
+from .translator.providers import GoogleTranslator, DeepLTranslator
+from .video_processor import VideoProcessorService
 
 
 class ServiceRegistry:
     def __init__(self, config: AppConfig) -> None:
         self.config = config
+
+    def audio_extractor(self) -> AudioExtractorService:
+        return AudioExtractorService()
+
+    def transcriber(self) -> TranscriberService | None:
+        if self.config.transcriber[0] == "whisper":
+            provider = WhisperTranscriber(
+                WhisperTranscriber.Variant[self.config.transcriber[1].lower()],
+                self.config.device,
+            )
+            return TranscriberService(provider)
+
+        else:
+            return None
+
+    def subtitle_generator(self):
+        return SubtitleGeneratorService()
+
+    def translate(self) -> TranslatorService | None:
+        if self.config.translator[0] == "google":
+            provider = GoogleTranslator()
+            return TranslatorService(provider)
+
+        elif self.config.translator[0] == "deepl":
+            provider = DeepLTranslator(self.config.translator[1])
+            return TranslatorService(provider)
+
+        else:
+            return None
+
+    def video_processor(self):
+        return VideoProcessorService()
